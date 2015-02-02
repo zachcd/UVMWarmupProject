3877436cd306d7c90ddb4ced25a0a5f20275c099
jdcarteruvm <jdcarter@uvm.edu>
2015-02-01 21:25:54 -0500

    Create BoardType.java

---
+++ BoardType.java
@@ -0,0 +1,13 @@
+/**************************************
+ CS205 Warmup Project 
+ 	Jonathan Carter
+ 	Zachary Chay-Dolan
+ 	Joseph Corbett
+ 	Aaron Brunette
+	
+	enum to keep track of game board type
+ **************************************/
+
+public enum BoardType {
+	TRIANGLE, ENGLISH
+}
