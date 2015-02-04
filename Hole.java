de86015497362a0e570bcb9a53a8947d3b9c25b5
Zachary Chay-Dolan <Zacharia.cd@gmail.com>
2015-02-03 21:56:11 -0500

    Constructor, move() and isValid() first try

--- Hole.java
+++ Hole.java
@@ -32,26 +32,59 @@
      FILLED, EMPTY, NONE
  }
  
+public class GameBoard{
+	
+	public GameBoard(BoardType type) {
+		
+		if (type== BoardType.TRIANGLE) {
+		Hole[] board;
 
-Hole[] board;
+		board = new Hole[5][5];
+		    
+		 board [0][0] = Hole.FILLED;
+		 board [0][1] = Hole.EMPTY;
+		 board [1][1] = Hole.NONE;
+		 
 
-board = new Hole[5][5];
-    
- board [0][0] = FILLED;
- board [0][1] = EMPTY;
- board [1][1] = NONE;
- 
+		for (int i = 0; i < 5; i++) {
+		    for (int j = 0; j < 5; j++) {
+		        board[i][j] = FILLED;
+		    }
+		    for (int k = j; j < 5; k < 5; k++) {
+		        board[i][k] = NONE;
+		    }
+		    }
+		       
+		board[2][1] = EMPTY;
+		} else {
+	         //ENGLISHBOARD  
+	           
+	          
+	         
+			 for (int i = 0; i < 7; i++) {
+				 for (int j = 0; j < 7; j++) {
+			 }
+			         board[i][j] = FILLED;
+			    }
+			    for (int i = 0; i < 2; i++) {
+			     for (int j = 0; j < 2; j++) {
+			         board[i][j] = NONE;
+			         board[i][6-j] = NONE;
+			         board[6-i][j] = NONE;
+			         board[6-i][6-j] = NONE;
+			     }
+			         
+			 
+			     }
+			             }
+			     
+			            board[3][3] = EMPTY;
+		
+		}
+		
+		
+	}
 
-for (int i = 0; i < 5; i++) {
-    for (int j = 0; j < 5; j++) {
-        board[i][j] = FILLED;
-    }
-    for (int k = j; j < 5; k < 5; k++) {
-        board[i][k] = NONE;
-    }
-    }
-       
-board[2][1] = EMPTY;
 
            
            
@@ -79,59 +112,36 @@ public bool move(int startx, int starty, int destx, int desty) {
 }
    
    
-   
-   
-        ENGLISHBOARD
+
    
            
        
-           
-           
-          
- //           
-// for (int i = 0; i < 7; i++) {
-//     for (int j = 0; j < 7; j++) {
-//         board[i][j] = FILLED;
-//     }
-//     for (int i = 0; i < 2; i++) {
-//     for (int j = 0; j < 2; j++) {
-//         board[i][j] = NONE;
-//         board[i][6-j] = NONE;
-//         board[6-i][j] = NONE;
-//         board[6-i][6-j] = NONE;
-//     }
-//         
-// 
-//     }
-//         
-//     }
-//     
-//            board[3][3] = EMPTY;
-//            
+
+            
            
   
            
            
-   private bool isValid(sx, sy, dx, dy) {
+   private bool isValid(int startx, int starty, int destx,int desty) {
        if(this.type) == ENGLISH) {
-    if(sx != dx && sy != dy)
+    if(startx != destx && starty != desty){
         return false;
        }
     
-  if(board[sx][sy] == FILLED &&
-   board[dx][dy] == EMPTY &&
-   board[(sx + dx) / 2][(sy + dy) /2] == FILLED)
-   
-   return true;
-   
+  if(board[startx][starty] == FILLED &&
+   board[destx][desty] == EMPTY &&
+   board[(startx + destx) / 2][(starty + desty) /2] == FILLED){
+	  return true;
+  }   
    else
        return false;
+   }
    }
           
           
 public bool isDone() {
-    for(int i = 0; i < 5; i++) {
-        for(int j = 0; j < 5; j++)
+    for(int i = 0; i < 7; i++) {
+        for(int j = 0; j < 7; j++)
         {
            if((isValid(i,j,i+2,j+2) || 
            (isValid(i,j,i+2,j) ||
@@ -151,4 +161,5 @@ public bool isDone() {
            
                
             
+}
 }
