7c27e626c4d1c66a670eeb6a7170e29203c3b7d4
Jonathan Carter <jdcarter@uvm.edu>
2015-02-03 22:57:22 -0500

    Fixed ==

--- GamePlayer.java
+++ GamePlayer.java
@@ -31,7 +31,7 @@ public class GamePlayer {
 									break;
 
 				case 'C': iface.showBoardMenu();
-									bt = iface.getChar("ET");
+									char bt = iface.getChar("ET");
 									switch(bt) 
 									{
 										case 'E': board = new GameBoard(BoardType.ENGLISH); break;
@@ -93,7 +93,7 @@ public class GamePlayer {
 			}
 			else if(move == "q")
 			{
-				quit == TRUE;
+				quit = TRUE;
 			}		
 		}
 		
